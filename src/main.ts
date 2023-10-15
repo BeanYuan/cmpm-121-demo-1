4e83d31e55d2c278e77e3bce3d5691e047dea70c
BeanYuan <65484019+BeanYuan@users.noreply.github.com>
2023-10-14 18:33:46 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,12 @@ header.innerHTML = gameName;
 app.append(header);
 
 let counter: number = 0;
+let growthRate: number = 0;
 let lastTimestamp: number = 0;
+
 const counterDisplay: HTMLParagraphElement = app.querySelector("#counterDisplay")!;
 const incrementButton: HTMLButtonElement = app.querySelector("#incrementButton")!;
+const upgradeButton: HTMLButtonElement = app.querySelector("#upgradeButton")!;
 
 function updateCounterDisplay() {
   counterDisplay.textContent = `${counter.toFixed(2)} 🍪`;
@@ -22,26 +25,46 @@ function updateCounterDisplay() {
 function incrementCounter() {
   counter++;
   updateCounterDisplay();
+  updateUpgradeButton();
 }
 
 function continuousCounter(timestamp: number) {
-  if (lastTimestamp === 0) {
+    if (lastTimestamp === 0) {
+        lastTimestamp = timestamp;
+    }
+    
+    const elapsedTime = timestamp - lastTimestamp;
+    const incrementAmount = (growthRate / 60) * (elapsedTime / 1000);
+
+    counter += incrementAmount;
+    updateCounterDisplay();
     lastTimestamp = timestamp;
-  }
 
-  const elapsedTime = timestamp - lastTimestamp;
-  const incrementAmount = (1 / 60) * (elapsedTime / 1000);
+    requestAnimationFrame(continuousCounter);
+}
 
-  counter += incrementAmount;
-  updateCounterDisplay();
-  lastTimestamp = timestamp;
+function updateUpgradeButton() {
+    if (counter >= 10) {
+      upgradeButton.disabled = false;
+    } else {
+      upgradeButton.disabled = true;
+    }
+}
 
-  requestAnimationFrame(continuousCounter);
+function upgrade() {
+    if (counter >= 10) {
+        counter -= 10; // Deduct 10 units
+        growthRate += 1; // Increment the growth rate by 1
+        updateCounterDisplay();
+        updateUpgradeButton();
+    }
 }
 
 incrementButton.addEventListener("click", incrementCounter);
+upgradeButton.addEventListener("click", upgrade);
 
 updateCounterDisplay();
+updateUpgradeButton();
 
 //setInterval(incrementCounter, 1000);
 
