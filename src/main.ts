598600062a7e13642e1321e29bfcf2e9df3078d3
BeanYuan <65484019+BeanYuan@users.noreply.github.com>
2023-10-14 18:15:07 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,12 @@ header.innerHTML = gameName;
 app.append(header);
 
 let counter: number = 0;
-const counterDisplay: HTMLParagraphElement =
-  app.querySelector("#counterDisplay")!;
-const incrementButton: HTMLButtonElement =
-  app.querySelector("#incrementButton")!;
+let lastTimestamp: number = 0;
+const counterDisplay: HTMLParagraphElement = app.querySelector("#counterDisplay")!;
+const incrementButton: HTMLButtonElement = app.querySelector("#incrementButton")!;
 
 function updateCounterDisplay() {
-  counterDisplay.textContent = `${counter} 🍪`;
+  counterDisplay.textContent = `${counter.toFixed(2)} 🍪`;
 }
 
 function incrementCounter() {
@@ -25,8 +24,25 @@ function incrementCounter() {
   updateCounterDisplay();
 }
 
+function continuousCounter(timestamp: number) {
+  if (lastTimestamp === 0) {
+    lastTimestamp = timestamp;
+  }
+
+  const elapsedTime = timestamp - lastTimestamp;
+  const incrementAmount = (1 / 60) * (elapsedTime / 1000);
+
+  counter += incrementAmount;
+  updateCounterDisplay();
+  lastTimestamp = timestamp;
+
+  requestAnimationFrame(continuousCounter);
+}
+
 incrementButton.addEventListener("click", incrementCounter);
 
 updateCounterDisplay();
 
-setInterval(incrementCounter, 1000);
+//setInterval(incrementCounter, 1000);
+
+requestAnimationFrame(continuousCounter);
