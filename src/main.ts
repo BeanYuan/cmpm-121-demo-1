3cebd83648d68f2b4c945104d6738c388239f4a2
BeanYuan <65484019+BeanYuan@users.noreply.github.com>
2023-10-14 17:41:04 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,20 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let counter: number = 0;
+const counterDisplay: HTMLParagraphElement = app.querySelector("#counterDisplay")!;
+const incrementButton: HTMLButtonElement = app.querySelector("#incrementButton")!;
+
+function updateCounterDisplay() {
+    counterDisplay.textContent = `${counter} 🍪`;
+}
+
+function incrementCounter() {
+    counter++;
+    updateCounterDisplay();
+}
+
+incrementButton.addEventListener("click", incrementCounter);
+
+updateCounterDisplay();
