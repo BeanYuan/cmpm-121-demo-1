5439836cef2e53e578fa67f3f777a2754c575e89
BeanYuan <65484019+BeanYuan@users.noreply.github.com>
2023-10-17 17:22:49 -0700

    scr link change

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,10 @@ let counter: number = 0;
 let growthRate: number = 0;
 let lastTimestamp: number = 0;
 
-const counterDisplay: HTMLParagraphElement = app.querySelector("#counterDisplay")!;
-const incrementButton: HTMLButtonElement = app.querySelector("#incrementButton")!;
+const counterDisplay: HTMLParagraphElement =
+  app.querySelector("#counterDisplay")!;
+const incrementButton: HTMLButtonElement =
+  app.querySelector("#incrementButton")!;
 const upgradeButton: HTMLButtonElement = app.querySelector("#upgradeButton")!;
 
 function updateCounterDisplay() {
@@ -29,35 +31,36 @@ function incrementCounter() {
 }
 
 function continuousCounter(timestamp: number) {
-    if (lastTimestamp === 0) {
-        lastTimestamp = timestamp;
-    }
-    
-    const elapsedTime = timestamp - lastTimestamp;
-    const incrementAmount = (growthRate / 60) * (elapsedTime / 1000);
-
-    counter += incrementAmount;
-    updateCounterDisplay();
+  if (lastTimestamp === 0) {
     lastTimestamp = timestamp;
+  }
 
-    requestAnimationFrame(continuousCounter);
+  const elapsedTime = timestamp - lastTimestamp;
+  const incrementAmount = (growthRate / 60) * (elapsedTime / 1000);
+
+  counter += incrementAmount;
+  updateCounterDisplay();
+  updateUpgradeButton();
+  lastTimestamp = timestamp;
+
+  requestAnimationFrame(continuousCounter);
 }
 
 function updateUpgradeButton() {
-    if (counter >= 10) {
-      upgradeButton.disabled = false;
-    } else {
-      upgradeButton.disabled = true;
-    }
+  if (counter >= 10) {
+    upgradeButton.disabled = false;
+  } else {
+    upgradeButton.disabled = true;
+  }
 }
 
 function upgrade() {
-    if (counter >= 10) {
-        counter -= 10; // Deduct 10 units
-        growthRate += 1; // Increment the growth rate by 1
-        updateCounterDisplay();
-        updateUpgradeButton();
-    }
+  if (counter >= 10) {
+    counter -= 10; // Deduct 10 units
+    growthRate += 1; // Increment the growth rate by 1
+    updateCounterDisplay();
+    updateUpgradeButton();
+  }
 }
 
 incrementButton.addEventListener("click", incrementCounter);
